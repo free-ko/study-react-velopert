7a634dac509509c5b89294412b0b0d0614719e6a
kyw <[duddnr654@naver.com]>
2020-08-29 15:40:52 +0900

    Practice with props

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import Say from './Say';
+// import Say from './Say';
 
-class App extends Component {
-  render() {
-    return <Say />;
-  }
+function Food({ name, hot }) {
+  return (
+    <div>
+      <h1>I Like {name}</h1>
+      <h2>Spicy is {hot}</h2>
+    </div>
+  );
+}
+
+const FoodILike = [
+  { name: 'kimchi', hot: 'strong' },
+  { name: 'kimbap', hot: 'soso' },
+];
+
+function App() {
+  return (
+    <div>
+      {FoodILike.map((dish) => (
+        <Food name={dish.name} hot={dish.hot} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
